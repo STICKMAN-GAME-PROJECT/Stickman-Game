082753c40db3c09607461b44ec596d3681376d6c
obayad <118033521+obayad@users.noreply.github.com>
2025-03-27 19:41:27 +0600

    Revert "necessary changes for character movement" to smaller one.
    This reverts commit 0dedf4a6ba152cd480ad4e4a7062f7c2441b658f.

--- Character.py
+++ Character.py
@@ -1,5 +1,4 @@
 import os
-import pygame
 
 '''
         ///--- This is the standing character svg file path ---///
@@ -15,8 +14,6 @@ stand5 = os.path.join(path_fighter_idle, "idle5.svg")
 stand6 = os.path.join(path_fighter_idle, "idle6.svg")
 stand7 = os.path.join(path_fighter_idle, "idle7.svg")
 stand8 = os.path.join(path_fighter_idle, "idle8.svg")
-
-stand_Right = [stand1, stand2, stand3, stand4, stand5, stand6, stand7, stand8]
 
 
 # ================================================================
@@ -37,26 +34,22 @@ walk6 = os.path.join(path_fighter_walk, 'walk6.svg')
 walk7 = os.path.join(path_fighter_walk, 'walk7.svg')
 walk8 = os.path.join(path_fighter_walk, 'walk8.svg')
 
-walk = [walk1, walk2, walk3, walk4, walk5, walk6, walk7, walk8]
-
 # ================================================================
 
 '''
         ///--- This is the running character svg file path ---///
 '''
 
-path_fighter_run = os.path.join('Assets', 'fighter_run')
+path_fighter_run = os.path.join('Assets', 'fighter_walk')
 
 
-run1 = os.path.join(path_fighter_run, 'run1.svg')
-run2 = os.path.join(path_fighter_run, 'run2.svg')
-run3 = os.path.join(path_fighter_run, 'run3.svg')
-run4 = os.path.join(path_fighter_run, 'run4.svg')
-run5 = os.path.join(path_fighter_run, 'run5.svg')
-run6 = os.path.join(path_fighter_run, 'run6.svg')
-run7 = os.path.join(path_fighter_run, 'run7.svg')
-run8 = os.path.join(path_fighter_run, 'run8.svg')
-
-run = [run1, run2, run3, run4, run5, run6, run7, run8]
+run1 = os.path.join(path_fighter_run, 'fg_run_1.svg')
+run2 = os.path.join(path_fighter_run, 'fg_run_2.svg')
+run3 = os.path.join(path_fighter_run, 'fg_run_3.svg')
+run4 = os.path.join(path_fighter_run, 'fg_run_4.svg')
+run5 = os.path.join(path_fighter_run, 'fg_run_5.svg')
+run6 = os.path.join(path_fighter_run, 'fg_run_6.svg')
+run7 = os.path.join(path_fighter_run, 'fg_run_7.svg')
+run8 = os.path.join(path_fighter_run, 'fg_run_8.svg')
 
 # ================================================================
