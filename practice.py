f6d7d3c1780e6a053702ca0797a115706c9da7d0
obayad <118033521+obayad@users.noreply.github.com>
2025-03-29 21:55:08 +0600

    Add road & background buildings assets

--- practice.py
+++ practice.py
@@ -1,118 +1,180 @@
-import Character as c
-import os
 import pygame
-pygame.init()
-
+import Character as c
+import math
 
 Clock = pygame.time.Clock()
 
 
-HEIGHT, WIDTH = 400, 800
+''''character init section'''
 
-x, y = 20, 350
-speed = 5
-is_jump = False  # is the character is jumping
-jump_right = 10
-walk_left = False
-walk_right = False
-walk_count = 0
-value = 0
+''''--------------------------------'''
 
-# character_walk = pygame.image.load(os.path.join("Assets", "fighter_walk", "walk1.svg"))
-
-character_walk = [pygame.image.load(f"Assets/fighter_walk/walk{i}.svg") for i in range(1, 9)]
-
-# character_walk_right = pygame.transform.scale(character_walk, (512, 512))
-# character_walk_left = pygame.transform.flip(character_walk_right, True,  False)
-
-win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Stickman")
-
-run = True
+'''color section'''
+WHITE = (255, 255, 255)
+'''---------------------------'''
 
 
-def draw_BG_gameWindow():
-    # win.blit(background_image, (0, 0))
-    pass
+class PyGame:
+    def __init__(self):
+        pygame.init()
+        self.is_jump = False
+        self.HEIGHT, self.WIDTH = 500, 800
+        self.x, self.y = 20, 340
+        self.speed = 10
+        self.jump_right = 10
+        self.walk_left = False
+        self.walk_right = False
+        self.walk_count = 0
 
+        self.scroll = 0  # Added scroll as an instance variable
+        self.camera_x = 0  # Camera position
 
-def Character_walk():
-    pass
+        self.value = 0
+        self.character_idle = [pygame.image.load(f"Assets/fighter_idle/idle{i}.svg") for i in range(1, 9)]
+        self.character_walk = [pygame.image.load(f"Assets/fighter_walk/walk{i}.svg") for i in range(1, 9)]
+        self.character_run = [pygame.image.load(f"Assets/fighter_run/run{i}.svg") for i in range(1, 9)]
 
+        #load roads & backgrounds
+        self.road = pygame.image.load("Assets/roads/wall.png")
+        self.buildings = [pygame.image.load(f"Assets/buildings/{i}.png") for i in range(1, 6)]
+    def main(self):
 
-# char_walk = pygame.rect((70, 300), 70, 100)
+        run = True
+        pygame.time.delay(100)  # fps
 
+        win = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.display.set_caption("Stickman")
 
-def Character_stand():
-    pass
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
 
+            if self.value >= self.character_idle.__len__():
+                self.value = 0
 
-while (run):
-    pygame.time.delay(60)  # fps
+            if self.value >= self.character_walk.__len__():
+                self.value = 0
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+            if self.value >= self.character_run.__len__():
+                self.value = 0
 
-    if value >= len(character_walk):
-        value = 0
+            keys = pygame.key.get_pressed()
 
-    keys = pygame.key.get_pressed()
-    # if keys[pygame.K_d]:
-    #     if x == 770:
-    #         pass
-    #     else:
-    #         x += 10
-    if keys[pygame.K_LEFT]:
-        if x == 0:
-            pass
-        # elif (pygame.KMOD_RSHIFT):
-        #     x += 10
-        else:
-            if keys[pygame.K_RSHIFT] and x >= 30:
-                x -= 20
+            if keys[pygame.K_LEFT]:
+
+                if self.x == 20:
+                    pass
+
+                else:
+                    if keys[pygame.K_RSHIFT] and self.x >= 30:
+                        self.x -= 40
+                    else:
+                        self.walk_left = True
+                        self.camera_x -= self.speed  # Move camera left
+                        self.scroll += self.speed  # Move road right
+                        self.x -= self.speed
+                        self.walk_right = False
+
+            if keys[pygame.K_RIGHT]:
+                if self.x == 7700:
+                    pass
+                else:
+                    if keys[pygame.K_RSHIFT] and self.x < 740:
+                        self.x += 40
+                        self.camera_x += 40  # Move camera right
+                        self.scroll -= 10  # Move road left
+                    else:
+                        self.walk_right = True
+                        self.x += self.speed
+                        self.camera_x += self.speed  # Move camera right
+                        self.scroll -= self.speed  # Move road left
+                    self.walk_left = False
+
+            if self.is_jump != True:
+                if keys[pygame.K_UP]:
+                    self.is_jump = True
             else:
-                walk_left = True
-                x -= speed
-                walk_right = False
-    if keys[pygame.K_RIGHT]:
-        if x == 770:
-            pass
-        else:
-            if keys[pygame.K_RSHIFT] and x < 740:
-                x += 20
+                if self.jump_right >= -10:
+                    neg = 1
+                    if self.jump_right < 0:
+                        neg = -1
+                    self.y -= (self.jump_right**2)*.3 * neg
+                    self.jump_right -= 1
+                else:
+                    self.is_jump = False
+                    self.jump_right = 10
+
+            win.fill(WHITE)
+
+
+            char_idle = self.character_idle[self.value]
+            char_idle = pygame.transform.scale(char_idle, (100, 140))
+
+            char_walk = self.character_walk[self.value]
+            char_walk = pygame.transform.scale(char_walk, (100, 140))
+
+            char_run = self.character_run[self.value]
+            char_run = pygame.transform.scale(char_run, (100, 140))
+
+
+            # Scrolling buildings
+            building_width = self.buildings[0].get_width()
+            tiles = math.ceil(self.WIDTH / building_width)
+
+            for i in range(tiles):
+                win.blit(self.buildings[0], (i * building_width + self.scroll - self.camera_x, 0))
+                win.blit(self.buildings[1], (i * building_width + self.scroll - self.camera_x, 0))
+                win.blit(self.buildings[2], (i * building_width + self.scroll - self.camera_x, 0))
+                win.blit(self.buildings[3], (i * building_width + self.scroll - self.camera_x, 0))
+                win.blit(self.buildings[4], (i * building_width + self.scroll - self.camera_x, 0))
+
+
+            # Scrolling road
+            road_width = self.road.get_width()
+            tiles = math.ceil(self.WIDTH / road_width)
+
+            for i in range(tiles):
+                win.blit(self.road, (i * road_width + self.scroll - self.camera_x, 260))
+
+            # Reset scroll to prevent overflow
+            if self.scroll > road_width:
+                self.scroll = 0
+            elif self.scroll < -road_width:
+                self.scroll = 0
+
+            # Adjust player position relative to camera
+            player_screen_x = 20  # Keep player in the center
+
+            # road = pygame.transform.scale(self.road, (2000, 240))
+            # win.blit(road, (0, 260))
+
+            if keys[pygame.K_LEFT] == False and keys[pygame.K_RIGHT] == False:
+                if self.walk_left == True:
+                    char_idle = pygame.transform.flip(char_idle, True, False)
+                win.blit(char_idle, (player_screen_x, self.y))
+
+            elif keys[pygame.K_RSHIFT]==True:
+                if keys[pygame.K_LEFT]==True:
+                    char_run = pygame.transform.flip(char_run, True, False)
+                    win.blit(char_run, (player_screen_x, self.y))
+                else:
+                    win.blit(char_run, (player_screen_x, self.y))
+
             else:
-                walk_right = True
-                x += speed
-            walk_left = False
-    if is_jump == False:
-        if keys[pygame.K_UP]:
-            is_jump = True
-    else:
-        if jump_right >= -10:
-            neg = 1
-            if jump_right < 0:
-                neg = -1
-            y -= (jump_right**2)*.5 * neg
-            jump_right -= 1
-        else:
-            is_jump = False
-            jump_right = 10
+                if keys[pygame.K_LEFT]==True:
+                    char_walk = pygame.transform.flip(char_walk, True, False)
+                    win.blit(char_walk, (player_screen_x, self.y))
+                else:
+                    win.blit(char_walk, (player_screen_x, self.y))
+            
 
-    win.fill((150, 10, 0))  # remove this after applying the bg
-    # pygame.draw.rect(win, (255, 255, 255), (x, y, width_rect, height_rect))
-    # remove the previous rect if the character is set to the window
+            self.value += 1
+            pygame.display.update()
+            Clock.tick(10)
+        pygame.quit()
 
-    ch_walk = character_walk[value]
-    ch_walk = pygame.transform.scale(ch_walk, (512, 512))  
-    win.blit(ch_walk, (0, 0))
 
-    pygame.display.update()
-    Clock.tick(60)
-
-    value+=1
-
-draw_BG_gameWindow()
-Character_walk()
-
-pygame.quit()
+if __name__ == "__main__":
+    pyg = PyGame()
+    pyg.main()
