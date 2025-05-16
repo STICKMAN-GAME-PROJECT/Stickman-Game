8f9cef0836e5a97df66a4a547e419d55e2a77ff3
obayad <118033521+obayad@users.noreply.github.com>
2025-05-16 16:38:08 +0600

    Update combo sprites to png

--- Character.py
+++ Character.py
@@ -6,14 +6,14 @@ import os
 
 path_fighter_idle = os.path.join('Assets', 'fighter_idle')
 
-stand1 = os.path.join(path_fighter_idle, "idle1.svg")
-stand2 = os.path.join(path_fighter_idle, "idle2.svg")
-stand3 = os.path.join(path_fighter_idle, "idle3.svg")
-stand4 = os.path.join(path_fighter_idle, "idle4.svg")
-stand5 = os.path.join(path_fighter_idle, "idle5.svg")
-stand6 = os.path.join(path_fighter_idle, "idle6.svg")
-stand7 = os.path.join(path_fighter_idle, "idle7.svg")
-stand8 = os.path.join(path_fighter_idle, "idle8.svg")
+stand1 = os.path.join(path_fighter_idle, "fighter_Idle_1.png")
+stand2 = os.path.join(path_fighter_idle, "fighter_Idle_2.png")
+stand3 = os.path.join(path_fighter_idle, "fighter_Idle_3.png")
+stand4 = os.path.join(path_fighter_idle, "fighter_Idle_4.png")
+stand5 = os.path.join(path_fighter_idle, "fighter_Idle_5.png")
+stand6 = os.path.join(path_fighter_idle, "fighter_Idle_6.png")
+stand7 = os.path.join(path_fighter_idle, "fighter_Idle_7.png")
+stand8 = os.path.join(path_fighter_idle, "fighter_Idle_8.png")
 
 stand_Right = [stand1, stand2, stand3, stand4, stand5, stand6, stand7, stand8]
 
@@ -27,14 +27,14 @@ stand_Right = [stand1, stand2, stand3, stand4, stand5, stand6, stand7, stand8]
 path_fighter_walk = os.path.join('Assets', 'fighter_walk')
 
 
-walk1 = os.path.join(path_fighter_walk, 'walk1.svg')
-walk2 = os.path.join(path_fighter_walk, 'walk2.svg')
-walk3 = os.path.join(path_fighter_walk, 'walk3.svg')
-walk4 = os.path.join(path_fighter_walk, 'walk4.svg')
-walk5 = os.path.join(path_fighter_walk, 'walk5.svg')
-walk6 = os.path.join(path_fighter_walk, 'walk6.svg')
-walk7 = os.path.join(path_fighter_walk, 'walk7.svg')
-walk8 = os.path.join(path_fighter_walk, 'walk8.svg')
+walk1 = os.path.join(path_fighter_walk, 'fighter_walk_1.png')
+walk2 = os.path.join(path_fighter_walk, 'fighter_walk_2.png')
+walk3 = os.path.join(path_fighter_walk, 'fighter_walk_3.png')
+walk4 = os.path.join(path_fighter_walk, 'fighter_walk_4.png')
+walk5 = os.path.join(path_fighter_walk, 'fighter_walk_5.png')
+walk6 = os.path.join(path_fighter_walk, 'fighter_walk_6.png')
+walk7 = os.path.join(path_fighter_walk, 'fighter_walk_7.png')
+walk8 = os.path.join(path_fighter_walk, 'fighter_walk_8.png')
 
 walk = [walk1, walk2, walk3, walk4, walk5, walk6, walk7, walk8]
 
@@ -47,14 +47,14 @@ walk = [walk1, walk2, walk3, walk4, walk5, walk6, walk7, walk8]
 path_fighter_run = os.path.join('Assets', 'fighter_run')
 
 
-run1 = os.path.join(path_fighter_run, 'run1.svg')
-run2 = os.path.join(path_fighter_run, 'run2.svg')
-run3 = os.path.join(path_fighter_run, 'run3.svg')
-run4 = os.path.join(path_fighter_run, 'run4.svg')
-run5 = os.path.join(path_fighter_run, 'run5.svg')
-run6 = os.path.join(path_fighter_run, 'run6.svg')
-run7 = os.path.join(path_fighter_run, 'run7.svg')
-run8 = os.path.join(path_fighter_run, 'run8.svg')
+run1 = os.path.join(path_fighter_run, 'fighter_run_1.png')
+run2 = os.path.join(path_fighter_run, 'fighter_run_2.png')
+run3 = os.path.join(path_fighter_run, 'fighter_run_3.png')
+run4 = os.path.join(path_fighter_run, 'fighter_run_4.png')
+run5 = os.path.join(path_fighter_run, 'fighter_run_5.png')
+run6 = os.path.join(path_fighter_run, 'fighter_run_6.png')
+run7 = os.path.join(path_fighter_run, 'fighter_run_7.png')
+run8 = os.path.join(path_fighter_run, 'fighter_run_8.png')
 
 run = [run1, run2, run3, run4, run5, run6, run7, run8]
 
@@ -66,32 +66,30 @@ run = [run1, run2, run3, run4, run5, run6, run7, run8]
 
 path_fighter_combo = os.path.join('Assets', 'fighter_combo')
 
-combo1 = os.path.join(path_fighter_combo, "combo_1.svg")
-combo2 = os.path.join(path_fighter_combo, "combo_2.svg")
-combo3 = os.path.join(path_fighter_combo, "combo_3.svg")
-combo4 = os.path.join(path_fighter_combo, "combo_4.svg")
-combo5 = os.path.join(path_fighter_combo, "combo_5.svg")
-combo6 = os.path.join(path_fighter_combo, "combo_6.svg")
-combo7 = os.path.join(path_fighter_combo, "combo_7.svg")
-combo8 = os.path.join(path_fighter_combo, "combo_8.svg")
-combo9 = os.path.join(path_fighter_combo, "combo_9.svg")
-combo10 = os.path.join(path_fighter_combo, "combo_10.svg")
-combo11 = os.path.join(path_fighter_combo, "combo_11.svg")
-combo12 = os.path.join(path_fighter_combo, "combo_12.svg")
-combo13 = os.path.join(path_fighter_combo, "combo_13.svg")
-combo14 = os.path.join(path_fighter_combo, "combo_14.svg")
-combo15 = os.path.join(path_fighter_combo, "combo_15.svg")
-combo16 = os.path.join(path_fighter_combo, "combo_16.svg")
-combo17 = os.path.join(path_fighter_combo, "combo_17.svg")
-combo18 = os.path.join(path_fighter_combo, "combo_18.svg")
-combo19 = os.path.join(path_fighter_combo, "combo_19.svg")
+combo1 = os.path.join(path_fighter_combo, "fighter_combo_1.png")
+combo2 = os.path.join(path_fighter_combo, "fighter_combo_2.png")
+combo3 = os.path.join(path_fighter_combo, "fighter_combo_3.png")
+combo4 = os.path.join(path_fighter_combo, "fighter_combo_4.png")
+combo5 = os.path.join(path_fighter_combo, "fighter_combo_5.png")
+combo6 = os.path.join(path_fighter_combo, "fighter_combo_6.png")
+combo7 = os.path.join(path_fighter_combo, "fighter_combo_7.png")
+combo8 = os.path.join(path_fighter_combo, "fighter_combo_8.png")
+combo9 = os.path.join(path_fighter_combo, "fighter_combo_9.png")
+combo10 = os.path.join(path_fighter_combo, "fighter_combo_10.png")
+combo11 = os.path.join(path_fighter_combo, "fighter_combo_11.png")
+combo12 = os.path.join(path_fighter_combo, "fighter_combo_12.png")
+combo13 = os.path.join(path_fighter_combo, "fighter_combo_13.png")
+combo14 = os.path.join(path_fighter_combo, "fighter_combo_14.png")
+combo15 = os.path.join(path_fighter_combo, "fighter_combo_15.png")
+combo16 = os.path.join(path_fighter_combo, "fighter_combo_16.png")
+combo17 = os.path.join(path_fighter_combo, "fighter_combo_17.png")
+combo18 = os.path.join(path_fighter_combo, "fighter_combo_18.png")
+combo19 = os.path.join(path_fighter_combo, "fighter_combo_19.png")
 
 combo = [combo1, combo2, combo3, combo4, combo5, combo6, combo7, combo8, combo9, combo10,
          combo11, combo12, combo13, combo14, combo15, combo16, combo17, combo18, combo19]
 
-# for i in range(20):
-#     print(len(combo))
-#     break
+
 
 # ================================================================
 
